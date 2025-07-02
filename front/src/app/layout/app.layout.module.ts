0c208d321ea941b3bafcfed4905915cc57b95909
AzureAD\YOUSSEFAMHILE <amhile@oromecanica.com>
2025-07-02 11:09:38 +0100

    Refactor app component and styles to remove PrimeNGConfig usage and update SCSS imports

--- front/src/app/layout/app.layout.module.ts
+++ front/src/app/layout/app.layout.module.ts
@@ -1,21 +1,13 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputTextModule } from 'primeng/inputtext';
-import { SidebarModule } from 'primeng/sidebar';
-import { BadgeModule } from 'primeng/badge';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { InputSwitchModule } from 'primeng/inputswitch';
-import { RippleModule } from 'primeng/ripple';
 import { AppMenuComponent } from './app.menu.component';
-import { AppMenuitemComponent } from './app.menuitem.component';
-import { RouterModule } from '@angular/router';
 import { AppTopBarComponent } from './app.topbar.component';
 import { AppFooterComponent } from './app.footer.component';
 import { AppSidebarComponent } from "./app.sidebar.component";
 import { AppLayoutComponent } from "./app.layout.component";
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppMenuitemComponent } from './app.menuitem.component';
 
 @NgModule({ declarations: [
         AppMenuitemComponent,
@@ -28,14 +20,6 @@ import { AppLayoutComponent } from "./app.layout.component";
     exports: [AppLayoutComponent], 
     imports: [
         BrowserModule,
-        FormsModule,
-        BrowserAnimationsModule,
-        InputTextModule,
-        SidebarModule,
-        BadgeModule,
-        RadioButtonModule,
-        InputSwitchModule,
-        RippleModule,
         RouterModule
     ], 
     providers: [provideHttpClient(withInterceptorsFromDi())] })
